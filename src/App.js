79ffd49eec5834d99212702328f126362479706e
Mehboob Alam <2023se7@student.uet.edu.pk>
2024-02-19 00:21:27 +0500

    All Updates is done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './component/Navbar'
+import TextForm from './component/TextForm';
+
+function App() {
+  return (
+ <>
+ <Navbar NavTitle="Text Manipulater by Aps Developer"/>
+ <TextForm/>
+ </>
+  );
+}
+
+export default App;
